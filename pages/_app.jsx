0e1bbd8680e7b035135cc2e635313cd6e0cd1bb3
hüseyin aslan <huseyinaslan@192.168.1.100>
2024-08-22 18:03:09 +0300

    ekledim

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,15 @@
-/* import "@/app/globals.css"; */
+import {fuchsia} from "tailwindcss/colors";
 import"@/app/globals.css";
+import { ThemeProvider } from "next-themes";
+import NextNProgress from "nextjs-progressbar";
+
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <>
+    <ThemeProvider attribute="class">
+      <NextNProgress color={fuchsia[500]} height={4}/>
       <Component {...pageProps} />
-    </>
+</ThemeProvider>
   );
 };
 
